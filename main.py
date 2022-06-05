851338b261ed50ad012505fcc5ba3c51782188f7
óchiAnthropos <aggyssss.dt2@gmail.com>
2022-06-05 17:43:48 +0300

    working on the start screen 0.1
    I made start screen screen
     Problems:
       1-screen without style
       2-elements of footer dont in right positions
        3-in scrol output img but mast to be new screen with some elements

--- main.py
+++ main.py
@@ -1,7 +1,96 @@
 from kivymd.app import MDApp
+from kivy.lang.builder import Builder
 
-class WordsBank(MDApp):
+
+kv = '''
+<MySwiper@MDSwiperItem>
+
+    FitImage:
+        source: "Data/Images/test_swiper.png"
+        radius: [6,]
+
+MDScreen:
+
+    BoxLayout:
+        id: toolbar
+        pos_hint: {"top": 1}
+        orientation: 'horizontal'
+        size_hint_y: 0.07
+        size_hint_x: 1
+
+
+
+        MDIconButton:
+            id: Settings
+            icon: "cog-outline"
+            user_font_size: "38sp"
+
+            pos_hint: {"left":1, 'center_x': 0.9}
+
+        MDLabel:
+            id: Random_word
+            padding: [0,10]
+            size_hint: 0.6, 1
+            text: "wordaadfafaafsfaafsfafasfsaff"
+            font_style: "H4"
+                
+            pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+
+
+        MDIconButton:
+            id: Localization
+            icon: "earth"
+
+            user_font_size: "38sp"
+            pos_hint: {"right":1, 'center_x': 0.9}
+
+
+
+    MDSwiper:
+        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+        size_hint_y: 0.8
+        height: root.height - toolbar.height - dp(40)
+        y: root.height - self.height - toolbar.height - dp(20)
+
+        MySwiper:
+
+        MySwiper:
+
+    BoxLayout:
+        id: toolbar
+        pos_hint: {"bottom": 1}
+        orientation: 'horizontal'
+        size_hint_y: 0.07
+        size_hint_x: 1
+
+        MDIconButton:
+            id: Statistic
+            icon: "chart-areaspline"
+            user_font_size: "38sp"
+
+            pos_hint: {"left":1}
+
+        MDIconButton:
+            id: Books
+            icon: "bookshelf"
+            user_font_size: "38sp"
+
+            pos_hint: {'center_y': 0.5, 'center_x': 0.5}
+
+        MDIconButton:
+            id: Edit
+            icon: "contrast"
+            user_font_size: "38sp"
+
+            pos_hint: {"right":1}
+
+
+
+'''
+
+
+class Main(MDApp):
     def build(self):
-        return
+        return Builder.load_string(kv)
 
-WordsBank().run()
+Main().run()
