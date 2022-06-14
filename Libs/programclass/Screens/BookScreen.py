31e4622c76180074e00a960b1cda29bb543d39c0
óchiAnthropos <aggyssss.dt2@gmail.com>
2022-06-14 21:14:34 +0300

    Betta Internal Screen Complite without aditing env
    I have worked with event to internal screen add popup to him and stiling also as like as book creen popup menu
    Loading in to scrren working normal
    1) need write funk for edit and add word into book

--- Libs/programclass/Screens/BookScreen.py
+++ Libs/programclass/Screens/BookScreen.py
@@ -9,6 +9,10 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.popup import Popup
 
+from Libs.programclass.modules.CustomList import CustomList
+from Libs.programclass.modules.WordsList import WordsList
+
+
 from kivy.properties import StringProperty
 
 
@@ -32,8 +36,62 @@ class BooksScreen(Screen):
             """INSERT INTO 'Data_name_of_books' VALUES (?,?)"""
             ]
 
+    #start swap to interanal zone
+    def swap_screen(self,name):
+        setattr(self.parent,"current" ,'bookInternal')
+        self.clear_internal_screen()
+        self.load_words(name)
 
 
+    def clear_internal_screen(self):
+        ob = self.parent.get_screen("bookInternal").ids.list_view
+        ob.remove_widget(ob.children[0])
+        ob.add_widget(WordsList())
+
+    # start algortitm for loading words > build_screen > add_word_into_internal > (get screen with words list)
+    def load_words(self, table_name):
+        if self.is_tabele_with_name(table_name):
+            conn = sqlite3.connect("Data/Base/Books.db")
+            cursor = conn.cursor()
+            print(table_name[0])
+            data = cursor.execute("SELECT * FROM " + '\''+table_name[0]+'\'')
+            data = data.fetchall()
+            conn.close()
+            self.build_screen(data)
+
+    # check data base if exist table
+    def is_tabele_with_name(self,name):
+        conn = sqlite3.connect("Data/Base/Books.db")
+        cursor = conn.cursor()
+        for item in cursor.execute("SELECT db_name FROM Data_name_of_books").fetchall():
+            if name == item:
+                conn.close()
+                return True
+            else:
+                continue
+
+
+    def build_screen(self,data):
+        pprint(data)
+        for word in data:
+            self.add_word_into_internal(word)
+
+    # genetate word list for interaln word menu
+    def add_word_into_internal(self,word):
+        but = CustomList()
+        but.Label_menu_texts['word'] = word[0]
+        but.Label_menu_texts['translate'] = word[2]
+
+        if word[1] != None and word[1] !='':
+            but.Label_menu_texts['transcription'] = word[1]
+
+        but.Label_menu_texts['status'] = str(word[4])
+
+        if word[3] != None and word[1] != '':
+            but.Label_menu_texts['asociations'] = word[3]
+
+        self.root.get_screen("bookInternal").ids.list_view.children[0].add_widget(but)
+    #end swap to interanal zone
 
     def cancel(self, *arg):
         self.NewDialog.dismiss()
@@ -45,7 +103,8 @@ class BooksScreen(Screen):
     def add_book_func(self, book):   
         but = ThreeLineAvatarIconListItem(
             text="{}".format( book[0] ),
-            secondary_text="{} words in it".format( book[1] )
+            secondary_text="{} words in it".format( book[1] ),
+            on_release= lambda x: self.swap_screen(book[0])
         )
 
         del_item = IconLeftWidget(
@@ -60,12 +119,12 @@ class BooksScreen(Screen):
         but.add_widget(edit_item)
         self.ids.books_add_list.add_widget(but)
 
-
+    #delete book form DATA BASE and from screen
     def del_book_func(self,bookName,wd_for_del):
         conn = sqlite3.connect("Data/Base/Books.db")
         cursor = conn.cursor()
         cursor.execute("DELETE FROM 'Data_name_of_books' WHERE db_name=?",[bookName])
-        cursor.execute("DROP TABLE IF EXISTS {}".format(bookName)) 
+        cursor.execute("DROP TABLE IF EXISTS '{}'".format(bookName)) 
         conn.commit()
         conn.close()
         self.ids.books_add_list.remove_widget(wd_for_del)
@@ -89,45 +148,58 @@ class BooksScreen(Screen):
         if self.text_input.text == "":
             self.text_input.error = True
         else:
-            self.create_new_book_by_name(self.text_input.text)
+
+            # zone for check to validation
+            name = self.text_input.text #.replace(" ",'')
+            self.create_new_book_by_name(name)
             self.NewDialog.dismiss()
 
-
+    #create new dialog for add new book into book screen
     def add_book_dialog(self, *instance):
         self.NewDialog = Popup(
-            title="Input a name of new dict",
+            title="Input a new book",
             size_hint=(None,None),
-            size=(400,300),
-            auto_dismiss=False
+            title_align='center',
+            size=(200,150),
+            auto_dismiss=False,
+            separator_color='#FEF9F5'
         )
         main_box = BoxLayout(
             orientation="vertical",
             size_hint=(0.95,0.95)
         )
         in_main_box = BoxLayout(
-            orientation="horizontal",
-            size_hint=(0.9,0.4)
+            size_hint=(0.9,0.65)
         )
         self.text_input = MDTextField(
-            size_hint=(0.9,0.6),
             helper_text="Name is empty, please input or close",
             helper_text_mode="on_error",
+            hint_text="write a book",
+            current_hint_text_color='#706B67',
+            foreground_color="#FEF9F5"
         )
 
         close_but = MDFlatButton(
-            size_hint=(0.4,0.45),
+            pos_hint={ 'center_x':0.5,'center_y':0.5 },
+            text_color="#FEF9F5",
             text="CLOSE"
         )
         ok_but = MDFlatButton(
-            size_hint=(0.4,0.45),
+            pos_hint={ 'center_x':0.5,'center_y':0.5 },
+            text_color="#FEF9F5",
             text="OK"
         )
         close_but.bind(on_release=self.cancel)
         ok_but.bind(on_release=self.create)
-        in_main_box.add_widget(ok_but)
-        in_main_box.add_widget(close_but)
-        main_box.add_widget(self.text_input)
+
+
+        bot_lay = BoxLayout(orientation='horizontal',pos_hint={'bottom':1},size_hint=(0.9,0.35))
+
+        bot_lay.add_widget(ok_but)
+        bot_lay.add_widget(close_but)
+        in_main_box.add_widget(self.text_input)
         main_box.add_widget(in_main_box)
+        main_box.add_widget(bot_lay)
 
         self.NewDialog.add_widget(main_box)
 
--- Libs/programclass/modules/CustomList.py
+++ Libs/programclass/modules/CustomList.py
@@ -1,5 +1,59 @@
+
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import VariableListProperty,ColorProperty,DictProperty,StringProperty
+from kivymd.font_definitions import theme_font_styles
+from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.button import MDFlatButton
+
+from kivy.uix.popup import Popup
+
+from pprint import pprint
 
 
 class CustomList(BoxLayout):
-    pass
+    WordsInterval = VariableListProperty([20,20])
+    text_color_miss = ColorProperty("#000D26")
+    
+    Label_font_style = StringProperty('Subtitle2')
+    Label_font_size = StringProperty("17")
+
+    Label_menu_texts = DictProperty({
+            "word": '[miss]',
+            "translate": '[miss]',
+            "transcription": '[miss]',
+            "status": '[miss]',
+            "asociations": '[miss]',
+        })
+
+
+    def reset_word_menu(self,*instanse):
+        pass
+
+    def reset_transcription_menu(self,*instanse):
+        pass
+
+    def reset_asociations_menu(self,*instanse):
+        pass
+
+    def reset_status_menu(self,*instanse):
+        pass
+
+    def reset_translate_menu(self,*instanse):
+        pass
+
+    def rewrite_word(self,word):
+        # word.children = ['',
+        # "[ref=''][miss][/ref]",
+        # "[ref='']0[/ref]",
+        # "[ref=''][miss][/ref]",
+        # "[ref='']дерево[/ref]",
+        # 'new tree']
+        print(word.children[-1].text)
+
+
+    def close(self, *instanse):
+        self.reset_menu.dismiss()
+
+
+    def create(self, *instanse):
+        self.reset_menu.dismiss()
